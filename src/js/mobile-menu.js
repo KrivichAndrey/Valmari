83a2dd46ccc61b0a7333544bde29254002b571ec
Andrey_Krivich <109085432+KrivichAndrey@users.noreply.github.com>
2023-06-19 08:51:26 +0300

    feat(header): implement mobile burger menu

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const openBtn = document.querySelector('.mobile-menu-open');
+  const menu = document.querySelector('.mobile-menu');
+  const menuBody = document.querySelector('.mobile-menu__body');
+  const closeBtn = document.querySelector('.mobile-menu-close');
+  const { body } = document;
+
+  function openMenu() {
+    menu.classList.add('active');
+    menuBody.classList.add('active');
+    body.classList.add('lock');
+  }
+
+  function closeMenu() {
+    menu.classList.remove('active');
+    menuBody.classList.remove('active');
+    body.classList.remove('lock');
+  }
+
+  if (openBtn) {
+    openBtn.addEventListener('click', () => {
+      openMenu();
+    });
+
+    menu.addEventListener('click', (e) => {
+      const { target } = e;
+      console.log(target);
+
+      if (target === closeBtn || target === menu) {
+        closeMenu();
+      }
+    });
+  }
+});
